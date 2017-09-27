97842c306c519edbde9076b2fcbabe83528a9c94
willcodeforcoffee <eric@hoffsoftcorp.com>
2017-09-27 08:25:37 -0600

    Fixes for new Typescript and for prod/NAS deploy (working)

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 # Build and deploy to Docker
+if [ -e .env ]
+then
+    set -o allexport
+    source .env
+    set +o allexport
+fi
+echo "Using DOCKER_HOST:"
+echo $DOCKER_HOST
+
 cd Hapthorn
-dotnet restore
-npm install
-dotnet publish -c Release -o ../out
+dotnet restore && npm install && dotnet publish -c Release -o ../out
 cd ..
-docker build .
+docker-compose build
---
+++ stop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Build and deploy to Docker
+if [ -e .env ]
+then
+    set -o allexport
+    source .env
+    set +o allexport
+fi
+echo "Using DOCKER_HOST:"  $DOCKER_HOST
+echo "DOCKER_API_VERSION:" $DOCKER_API_VERSION
+docker-compose down
