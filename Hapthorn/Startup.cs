fa46006e3576e4fbce86213f8a1b1eb2dbe1aa37
willcodeforcoffee <eric@hoffsoftcorp.com>
2017-10-24 10:00:46 -0600

    Removed unused using

--- Hapthorn/Startup.cs
+++ Hapthorn/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Npgsql.Logging;
 using Serilog;
 
 namespace Hapthorn
